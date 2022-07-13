1ed22e52e0cc4a17b676a8012816f79752ccdfe1
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-07-12 23:36:30 -0400

    Add pdf file describing duscussing some aspects of the code
    This commit also fixes some identation issues.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- TP1/matrix.s
+++ TP1/matrix.s
@@ -11,41 +11,41 @@ matrix_c:		.word32
 
 .code
 
-setup:			addi			$s3, $zero, 0x4		;; matrix_a/c number of rows (rows_a, rows_c)
-				addi			$s4, $zero, 0x2		;; matrix_b/c number of columns (cols_b, cols_c)
-				addi			$s5, $zero, 0x3		;; matrix_b number of rows (rows_b, cols_a)
-loopi:			bne				$s0, $s3,  upd_idx_j
-				b				done
+setup:			addi			$s3, $zero, 0x4	;; matrix_a/c number of rows (rows_a, rows_c)
+			addi			$s4, $zero, 0x2	;; matrix_b/c number of columns (cols_b, cols_c)
+			addi			$s5, $zero, 0x3	;; matrix_b number of rows (rows_b, cols_a)
+loopi:			bne			$s0, $s3,  upd_idx_j
+			b			done
 upd_idx_j:		movz			$s1, $zero, $zero	;; restart j index
-loopj:			bne				$s1, $s4, upd_idx_k
-				addi			$s0, $s0, 0x1
-				b				loopi
+loopj:			bne			$s1, $s4, upd_idx_k
+			addi			$s0, $s0, 0x1
+			b			loopi
 upd_idx_k:		movz			$s2, $zero, $zero	;; restart k index
-loopk:			bne				$s2, $s5, statement
-				addi			$s1, $s1, 0x1
-				b				loopj
-statement:		mult			$s0, $s5			;; w = i * cols_a
-				mflo			$s7
-				add				$s7, $s7, $s2		;; w = w + k
-				mult			$s7, $s3
-				mflo			$s7
-				lw				$s7, matrix_a($s7)	;; a[i * cols_a + k]
-				mult			$s2, $s4			;; t = k * cols_b
-				mflo			$t0
-				add				$t0, $t0, $s1		;; t = t + j
-				mult			$t0, $s3
-				mflo			$t0
-				lw				$t0, matrix_b($t0)	;; b[k * cols_b + j]
-				mult			$t0, $s7			;; a[i * cols_a + k] * b[k * cols_b + j]
-				mflo			$t0
-				mult			$s0, $s4			;; z = i * cols_c
-				mflo			$s7
-				add				$s7, $s7, $s1		;; z = z + j
-				mult			$s7, $s3			;; z = z * 0x4
-				mflo			$s7
-				lw				$s6, matrix_c($s7)
-				add				$s6, $s6, $t0
-				sw				$s6, matrix_c($s7)	;; c[i * cols_c + j] += a[i * cols_a + k] * b[k * cols_b + j]
-				addi			$s2, $s2, 0x1		;; update k index
-				b				loopk
-done:			syscall			0x0
+loopk:			bne			$s2, $s5, statement
+			addi			$s1, $s1, 0x1
+			b			loopj
+statement:		mult			$s0, $s5		;; w = i * cols_a
+			mflo			$s7
+			add			$s7, $s7, $s2		;; w = w + k
+			mult			$s7, $s3
+			mflo			$s7
+			lw			$s7, matrix_a($s7)	;; a[i * cols_a + k]
+			mult			$s2, $s4		;; t = k * cols_b
+			mflo			$t0
+			add			$t0, $t0, $s1		;; t = t + j
+			mult			$t0, $s3
+			mflo			$t0
+			lw			$t0, matrix_b($t0)	;; b[k * cols_b + j]
+			mult			$t0, $s7		;; a[i * cols_a + k] * b[k * cols_b + j]
+			mflo			$t0
+			mult			$s0, $s4		;; z = i * cols_c
+			mflo			$s7
+			add			$s7, $s7, $s1		;; z = z + j
+			mult			$s7, $s3		;; z = z * 0x4
+			mflo			$s7
+			lw			$s6, matrix_c($s7)
+			add			$s6, $s6, $t0
+			sw			$s6, matrix_c($s7)	;; c[i * cols_c + j] += a[i * cols_a + k] * b[k * cols_b + j]
+			addi			$s2, $s2, 0x1		;; update k index
+			b			loopk
+done:			syscall		0x0
--- TP1/matrix2.s
+++ TP1/matrix2.s
@@ -11,36 +11,36 @@ matrix_c:		.word32
 
 .code
 
-setup:			addiu			$s3, $zero, 0x4		;; matrix_a/c number of rows (rows_a, rows_c)
-				addiu			$s4, $zero, 0x2		;; matrix_b/c number of columns (cols_b, cols_c)
-				addiu			$s5, $zero, 0x3		;; matrix_b number of rows (rows_b, cols_a)
-loopi:			bne				$s0, $s3, upd_j
-				b				done
+setup:			addiu			$s3, $zero, 0x4	;; matrix_a/c number of rows (rows_a, rows_c)
+			addiu			$s4, $zero, 0x2	;; matrix_b/c number of columns (cols_b, cols_c)
+			addiu			$s5, $zero, 0x3	;; matrix_b number of rows (rows_b, cols_a)
+loopi:			bne			$s0, $s3, upd_j
+			b			done
 upd_j:			movz			$s1, $zero, $zero	;; restart j index
-loopj:			bne				$s1, $s4, upd_k
-				addiu			$s0, $s0, 0x1
-				b				loopi
+loopj:			bne			$s1, $s4, upd_k
+			addiu			$s0, $s0, 0x1
+			b			loopi
 upd_k:			movz			$s2, $zero, $zero	;; restart k index
-loopk:			bne				$s2, $s5, statement
-				addiu			$s1, $s1, 0x1
-				b				loopj
+loopk:			bne			$s2, $s5, statement
+			addiu			$s1, $s1, 0x1
+			b			loopj
 statement:		multu			$s0, $s5
-				mflo			$s7
-				addu			$s7, $s7, $s2
-				sll				$s7, $s7, 0x2
-				lw				$s7, matrix_a($s7)	;; a[i * cols_a + k]
-				sll				$t0, $s2, 0x1
-				addu			$t0, $t0, $s1
-				sll				$t0, $t0, 0x2
-				lw				$t0, matrix_b($t0)	;; b[k * cols_b + j]
-				sll				$t1, $s0, 0x1
-				addu			$t1, $t1, $s1
-				sll				$t1, $t1, 0x2
-				lw				$s6, matrix_c($t1)	;; c[i * cols_c + j]
-				mult			$t0, $s7
-				mflo			$t0
-				add				$s6, $s6, $t0		;; c[i * cols_c + j] += a[i * cols_a + k] * b[k * cols_b + j]
-				sw				$s6, matrix_c($t1)
-				addiu			$s2, $s2, 0x1		;; update k index
-				b				loopk
-done:			syscall			0x0
+			mflo			$s7
+			addu			$s7, $s7, $s2
+			sll			$s7, $s7, 0x2
+			lw			$s7, matrix_a($s7)	;; a[i * cols_a + k]
+			sll			$t0, $s2, 0x1
+			addu			$t0, $t0, $s1
+			sll			$t0, $t0, 0x2
+			lw			$t0, matrix_b($t0)	;; b[k * cols_b + j]
+			sll			$t1, $s0, 0x1
+			addu			$t1, $t1, $s1
+			sll			$t1, $t1, 0x2
+			lw			$s6, matrix_c($t1)	;; c[i * cols_c + j]
+			mult			$t0, $s7
+			mflo			$t0
+			add			$s6, $s6, $t0		;; c[i * cols_c + j] += a[i * cols_a + k] * b[k * cols_b + j]
+			sw			$s6, matrix_c($t1)
+			addiu			$s2, $s2, 0x1		;; update k index
+			b			loopk
+done:			syscall		0x0
